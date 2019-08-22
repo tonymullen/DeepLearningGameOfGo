f3b15466291177c3e5077c7a621d3b3972c023eb
Tony Mullen <tony.mullen@gmail.com>
2019-08-21 22:04:54 -0700

    chapter 3

---
+++ dlgo/agent/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 19:22:39 2019
+
+@author: tonymullen
+"""
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def select_move(self, game_state):
+        raise NotImplementedError()
---
+++ dlgo/utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 20:10:07 2019
+
+@author: tonymullen
+"""
+
+from dlgo import gotypes
+
+COLS = "ABCDEFGHJKLMNOPQRST"
+STONE_TO_CHAR = {
+        None: ' . ',
+        gotypes.Player.black: ' x ',
+        gotypes.Player.white: ' o ',
+    }
+
+
+def print_move(player, move):
+    if move.is_pass:
+        move_str = 'passes'
+    elif move.is_resign:
+        move_str = 'resigns'
+    else:
+        move_str = '%s%d' % (COLS[move.point.col - 1], move.point.row)
+    print('%s %s' % (player, move_str))
+
+
+def print_board(board):
+    for row in range(board.num_rows, 0, -1):
+        bump = ' ' if row <= 9 else ""
+        line = []
+        for col in range(1, board.num_cols + 1):
+            stone = board.get(gotypes.Point(row=row, col=col))
+            line.append(STONE_TO_CHAR[stone])
+        print('%s%d %s' % (bump, row, ''.join(line)))
+    print('    ' + '  '.join(COLS[:board.num_cols]))
+
+
+def point_from_coords(coords):
+    col = COLS.index(coords[0]) + 1
+    row = int(coords[1:])
+    return gotypes.Point(row=row, col=col)
